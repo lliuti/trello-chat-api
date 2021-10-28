0868edcac5dd76c500b10a526f8c2cf80b7054d8
Lucas Liuti <dev.lliuti@gmail.com>
2021-10-27 22:36:28 -0300

    Removing console.log and Specifying response type

--- src/services/NewMessageService.ts
+++ src/services/NewMessageService.ts
@@ -1,15 +1,14 @@
 import { io } from "../app";
 import { v4 as uuid } from "uuid";
+import { IMessage } from "../dtos/IMessage";
 
 class NewMessageService {
-  async execute(messageObject) {
-    const msgObj = {
+  async execute(messageObject): Promise<void> {
+    const msgObj: IMessage = {
       id: uuid(),
       message: messageObject.message,
       from: messageObject.from,
     };
-
-    console.log(msgObj);
 
     io.emit("new-message", msgObj);
   }
